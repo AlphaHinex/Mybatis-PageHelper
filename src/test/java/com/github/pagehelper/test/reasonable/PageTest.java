a845d9da0c467e959ef93468286bab2df08cb5bd
Alpha Hinex <AlphaHinex@gmail.com>
2021-11-05 17:41:25 +0800

    PageSizeZero null test case

--- src/test/java/com/github/pagehelper/test/reasonable/PageTest.java
+++ src/test/java/com/github/pagehelper/test/reasonable/PageTest.java
@@ -78,21 +78,21 @@ public class PageTest {
         try {
             //获取第20页，2条内容
             //分页插件会自动改为查询最后一页
-//            PageHelper.startPage(1, 0, true, true, null);
-//            List<User> list = userMapper.selectAll();
-//            PageInfo<User> page = new PageInfo<User>(list);
-//            assertEquals(0, list.size());
-//            assertEquals(0, page.getStartRow());
-//            assertEquals(1, page.getPageNum());
-//            assertEquals(183, page.getTotal());
-
-            PageHelper.startPage(1, Integer.MAX_VALUE, true, true, null);
+            PageHelper.startPage(1, 0, true, true, null);
             List<User> list = userMapper.selectAll();
             PageInfo<User> page = new PageInfo<User>(list);
             assertEquals(183, list.size());
             assertEquals(1, page.getStartRow());
             assertEquals(1, page.getPageNum());
             assertEquals(183, page.getTotal());
+
+//            PageHelper.startPage(1, Integer.MAX_VALUE, true, true, null);
+//            List<User> list = userMapper.selectAll();
+//            PageInfo<User> page = new PageInfo<User>(list);
+//            assertEquals(183, list.size());
+//            assertEquals(1, page.getStartRow());
+//            assertEquals(1, page.getPageNum());
+//            assertEquals(183, page.getTotal());
         } finally {
             sqlSession.close();
         }
